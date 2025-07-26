e4d2be0e2fcce0ecac0fd7d04a3c6789d82d274c
Matt Pears <121106164+MattPears1@users.noreply.github.com>
2025-07-26 15:26:37 +0100

    fix: revert notification positioning to handle mobile overflow

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -207,7 +207,8 @@ const Header: React.FC = () => {
                 </button>
                 {showNotifications && (
                   <div
-                    className="absolute right-0 mt-2 z-50 w-80 max-w-[calc(100vw-2rem)]"
+                    className="absolute right-0 mt-2 z-50 w-screen max-w-[calc(100vw-2rem)] sm:w-auto sm:max-w-none"
+                    style={{ right: 'max(-1rem, calc(-100vw + 100% + 2rem))' }}
                     onClick={(e) => e.stopPropagation()}
                   >
                     <NotificationCenter
